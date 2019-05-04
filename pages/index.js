71fd8d558fffd970e826b3b01fa808cf0bbd2e7e
qsat <araisat@gmail.com>
2019-05-05 04:11:27 +0900

    update content

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,18 @@
 import { withAmp } from 'next/amp'
+import Head from 'next/head'
 import Header from '../components/Header'
 
 const Index = () => (
   <div>
+    <Head>
+      <title>nowonair.info は様々なメディアのオンエア情報を提供しています</title>
+    </Head>
     <Header />
     <p>Hello World!!</p>
-    <p>Nextjs と AMPページのテストをしています</p>
+    <section>
+      <h1>Nextjs と AMPページのテストをしています</h1>
+      <p>Nextjs と AMPページのテストをしています</p>
+    </section>
   </div>
 )
 
