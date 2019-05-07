83231793285085ab52ab05a49175cd64f2807371
satoshi arai <01017830@CA3249.local>
2019-05-07 10:57:26 +0900

    feat(amp): about page

--- pages/about.js
+++ pages/about.js
@@ -1,9 +1,15 @@
+import { withAmp } from 'next/amp'
+import Head from 'next/head'
 import Header from '../components/Header'
-export default function About() {
-  return (
-    <div>
-      <Header />
-      <p>This is the about page</p>
-    </div>
-  )
-}
+
+const About = () => ( 
+  <div>
+    <Head>
+      <title>nowonair.info は様々なメディアのオンエア情報を提供しています</title>
+    </Head>
+    <Header />
+    <p>This is the about page</p>
+  </div>
+)
+
+export default withAmp(About)
